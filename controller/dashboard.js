0a5c26e393bb4eddfa808100cd09bcf7cc283e79
Ahmad Faqih Hidayat <ahmadfh36115@gmail.com>
2022-12-25 21:35:57 +0700

    BE & FE (pagination & nambahin require di form)

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -3,23 +3,33 @@ const router = express.Router();
 const Document = require("../model/document");
 const Category = require("../model/category");
 const { isAdmin } = require("../utils/auth");
-const { basedOnCategory, basedOnName } = require("../utils/filterAndSearch");
+const { basedOnCategory, basedOnName, documentDefault } = require("../utils/filterAndSearch");
 
 router.get("/", async (req, res) => {
   const { name: username, email: useremail, role: userrole } = req.user || {};
+  const categories = await Category.find();
+  const page = req.query.page ? parseInt(req.query.page) : 1;
+  const perPage = 3;
   let documents;
   let numOfResult;
+  let checkedPage;
+  let option;
   if (req.query.filter) {
-    documents = await basedOnCategory(req.query.filter);
-    numOfResult = documents.length;
+    documents = (await basedOnCategory(req.query.filter, perPage, page)).documentPerKategori;
+    numOfResult = (await basedOnCategory(req.query.filter, perPage, page)).numOfResult;
+    checkedPage = (await basedOnCategory(req.query.filter, perPage, page)).checkedPage;
+    option = `filter=${req.query.filter}`;
   } else if (req.query.search) {
-    documents = await basedOnName(req.query.search);
-    numOfResult = documents.length;
+    documents = (await basedOnName(req.query.search, perPage, page)).documentBasedOnName;
+    numOfResult = (await basedOnName(req.query.search, perPage, page)).numOfResult;
+    checkedPage = (await basedOnName(req.query.search, perPage, page)).checkedPage;
+    option = `search=${req.query.search}`;
   } else {
-    documents = await Document.find();
-    numOfResult = documents.length;
+    documents = (await documentDefault(perPage, page)).documentDefault;
+    numOfResult = (await documentDefault(perPage, page)).numOfResult;
+    checkedPage = (await documentDefault(perPage, page)).checkedPage;
   }
-  const categories = await Category.find();
+  const totalPages = Math.ceil(numOfResult / perPage);
   res.render("dashboard", {
     title: "Dashboard",
     layout: "layout/secondary-layout",
@@ -30,25 +40,38 @@ router.get("/", async (req, res) => {
     documents,
     categories,
     numOfResult,
+    current: req.query.page ? checkedPage : 1,
+    totalPages,
     isEditAndDelete: false,
+    option,
   });
 });
 
 router.get("/admin", isAdmin, async (req, res) => {
   const { name: username, email: useremail, role: userrole } = req.user || {};
+  const categories = await Category.find();
+  const page = req.query.page ? parseInt(req.query.page) : 1;
+  const perPage = 3;
   let documents;
   let numOfResult;
+  let checkedPage;
+  let option;
   if (req.query.filter) {
-    documents = await basedOnCategory(req.query.filter);
-    numOfResult = documents.length;
+    documents = (await basedOnCategory(req.query.filter, perPage, page)).documentPerKategori;
+    numOfResult = (await basedOnCategory(req.query.filter, perPage, page)).numOfResult;
+    checkedPage = (await basedOnCategory(req.query.filter, perPage, page)).checkedPage;
+    option = `filter=${req.query.filter}`;
   } else if (req.query.search) {
-    documents = await basedOnName(req.query.search);
-    numOfResult = documents.length;
+    documents = (await basedOnName(req.query.search, perPage, page)).documentBasedOnName;
+    numOfResult = (await basedOnName(req.query.search, perPage, page)).numOfResult;
+    checkedPage = (await basedOnName(req.query.search, perPage, page)).checkedPage;
+    option = `search=${req.query.search}`;
   } else {
-    documents = await Document.find();
-    numOfResult = documents.length;
+    documents = (await documentDefault(perPage, page)).documentDefault;
+    numOfResult = (await documentDefault(perPage, page)).numOfResult;
+    checkedPage = (await documentDefault(perPage, page)).checkedPage;
   }
-  const categories = await Category.find();
+  const totalPages = Math.ceil(numOfResult / perPage);
   const isEditAndDelete = userrole === "Admin" ? true : false;
   res.render("dashboard", {
     title: "Dashboard",
@@ -60,6 +83,10 @@ router.get("/admin", isAdmin, async (req, res) => {
     documents,
     categories,
     numOfResult,
+    current: req.query.page ? checkedPage : 1,
+    totalPages,
+    isEditAndDelete: false,
+    option,
     isEditAndDelete,
   });
 });
--- utils/filterAndSearch.js
+++ utils/filterAndSearch.js
@@ -1,10 +1,53 @@
 const Document = require("../model/document");
 
+const checkPage = function (page, numOfResult, perPage) {
+  if (isNaN(page)) {
+    return 1;
+  } else if (page < 1) {
+    return 1;
+  } else if (page > Math.ceil(numOfResult / perPage)) {
+    return Math.ceil(numOfResult / perPage);
+  } else {
+    return page;
+  }
+};
+
 module.exports = {
-  basedOnCategory: async function (category) {
-    return await Document.find({ filekategori: category });
+  basedOnCategory: async function (category, perPage, page) {
+    const numOfResult = await Document.find({ filekategori: category }).countDocuments();
+    const checkedPage = checkPage(page, numOfResult, perPage);
+    return {
+      documentPerKategori: await Document.find({ filekategori: category })
+        .sort({ _id: -1 })
+        .skip(perPage * (checkedPage - 1))
+        .limit(perPage)
+        .sort({ _id: -1 }),
+      numOfResult,
+      checkedPage,
+    };
   },
-  basedOnName: async function (name) {
-    return await Document.find({ filename: { $regex: name, $options: "i" } });
+  basedOnName: async function (name, perPage, page) {
+    const numOfResult = await Document.find({ filename: { $regex: name, $options: "i" } }).countDocuments();
+    const checkedPage = checkPage(page, numOfResult, perPage);
+    return {
+      documentBasedOnName: await Document.find({ filename: { $regex: name, $options: "i" } })
+        .sort({ _id: -1 })
+        .skip(perPage * (checkedPage - 1))
+        .limit(perPage),
+      numOfResult,
+      checkedPage,
+    };
+  },
+  documentDefault: async function (perPage, page) {
+    const numOfResult = await Document.find({}).countDocuments();
+    const checkedPage = checkPage(page, numOfResult, perPage);
+    return {
+      documentDefault: await Document.find({})
+        .sort({ _id: -1 })
+        .skip(perPage * (checkedPage - 1))
+        .limit(perPage),
+      numOfResult,
+      checkedPage,
+    };
   },
 };
